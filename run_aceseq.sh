9ca2e810d78ae4d92b1ea1b30cb92c6f40f50611
Kübra Narcı <kbrnrc@gmail.com>
2023-05-03 10:12:54 +0200

    small changes

--- run_aceseq.sh
+++ run_aceseq.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 module load nextflow/22.10.7
-nextflow run main.nf -profile dkfz_cluster_hg38,singularity --input seqc2_testdata_aceseq/samplesheet_38.csv -resume
+nextflow run main.nf -profile dkfz_cluster_hg38,singularity -resume
